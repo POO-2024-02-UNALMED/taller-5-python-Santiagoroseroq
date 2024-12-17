f75d093ae6e1e81d612bb26a2be06ec521ee2872
Santiagoroseroq <sroseroq@unal.edu.co>
2024-12-17 10:19:55 -0500

    Create Reptil.py

---
+++ .github/workflows/zooAnimales/Reptil.py
@@ -0,0 +1,27 @@
+# zooAnimales/reptil.py
+
+from zooAnimales.animal import Animal
+
+class Reptil(Animal):
+    iguanas = 0
+    serpientes = 0
+
+    def __init__(self, nombre, edad, genero, colorEscamas, largoCola, habitat, zona=None, zoo=None):
+        super().__init__(nombre, edad, genero, zona, zoo)
+        self.colorEscamas = colorEscamas
+        self.largoCola = largoCola
+        self.habitat = habitat
+
+    def crearIguana(self, nombre, edad, genero):
+        self.iguanas += 1
+        return Reptil(nombre, edad, genero, "verde", 3, "humedal")
+
+    def crearSerpiente(self, nombre, edad, genero):
+        self.serpientes += 1
+        return Reptil(nombre, edad, genero, "blanco", 1, "jungla")
+
+    def cantidadReptiles(self):
+        return Reptil.iguanas + Reptil.serpientes
+
+    def movimiento(self):
+        return "reptar"
