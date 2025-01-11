9a75bf3760095c7098a462ab2502729e0f73316e
Santiagoroseroq <sroseroq@unal.edu.co>
2025-01-11 08:26:01 -0500

    Update Animal.py

--- .github/workflows/zooAnimales/Animal.py
+++ .github/workflows/zooAnimales/Animal.py
@@ -1,35 +1,39 @@
-# zooAnimales/animal.py
+__package__
 from zooAnimales.Mamifero import Mamifero
 from zooAnimales.Ave import Ave
 from zooAnimales.Reptil import Reptil
 from zooAnimales.Pez import Pez
 from zooAnimales.Anfibio import Anfibio
 class Animal:
-    totalAnimales = 0  # Variable de clase para contar todos los animales creados.
+    totalAnimales = 0
+    totalPorTipo = {
+        "Mamíferos": 0,
+        "Aves": 0,
+        "Reptiles": 0,
+        "Peces": 0,
+        "Anfibios": 0
+    }
 
-    def __init__(self, nombre, edad, genero, zona=None, zoo=None):
+    def __init__(self, nombre, edad, habitat, genero, zona=None):
         self.nombre = nombre
         self.edad = edad
+        self.habitat = habitat
         self.genero = genero
         self.zona = zona
-        self.zoo = zoo
-        Animal.totalAnimales += 1  # Incrementa el contador global de animales.
+        Animal.totalAnimales += 1
+
+    def movimiento(self):
+        return "desplazarse"
 
     @classmethod
     def totalPorTipo(cls):
-        mamiferos = Mamifero.cantidadMamiferos()
-        aves = Ave.cantidadAves()
-        reptiles = Reptil.cantidadReptiles()
-        peces = Pez.cantidadPeces()
-        anfibios = Anfibio.cantidadAnfibios()
-        
-        return f"Mamíferos: {mamiferos}, Aves: {aves}, Reptiles: {reptiles}, Peces: {peces}, Anfibios: {anfibios}"
-
+        resultado = ""
+        for tipo, cantidad in cls.totalPorTipo.items():
+            resultado += f"• {tipo}: {cantidad}\n"
+        return resultado
 
     def __str__(self):
-        return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, " \
-               f"habito en {self.zona}, mi género es {self.genero}, " \
-               f"la zona en la que me ubico es {self.zona}, en el zoológico {self.zoo}."
-
-    def movimiento(self):
-        return "desplazarse"  # El movimiento por defecto para la clase Animal es "desplazarse".
+        if self.zona:
+            return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi género es {self.genero}, la zona en la que me ubico es {self.zona.nombre}, en el {self.zona.zoo.nombre}."
+        else:
+            return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi género es {self.genero}."
